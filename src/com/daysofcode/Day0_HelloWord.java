d16e1a5ded96a8cd7306d8959091b85586349698
Felipe Galindo <galindoaguilarf@gmail.com>
2020-11-10 07:18:35 -0600

    Hacker Rank Day 0

---
+++ src/com/daysofcode/Day0_HelloWord.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.daysofcode;
+
+/**
+ *
+ * @author anonimo
+ */
+public class Day0_HelloWord {
+    public static void main(String arg[]){
+        System.out.println("Welcome to 30 Days of Code!");
+    }
+}
