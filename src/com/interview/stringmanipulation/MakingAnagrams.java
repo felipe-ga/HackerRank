de9f6b80d80a097d280c088e057f4ccdab67993d
felipe-ga <galindoaguilarf@gmail.com>
2021-03-01 18:48:37 -0600

    Mark and Toys

--- src/com/interview/stringmanipulation/MakingAnagrams.java
+++ src/com/interview/stringmanipulation/MakingAnagrams.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.interview;
+package com.interview.stringmanipulation;
 
 import java.util.HashMap;
 import java.util.Map;
